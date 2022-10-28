94945b8a8964522862d4aa1edadbb16442d39a1b
Sidharth Nair <sidnair@utexas.edu>
2022-10-28 17:37:40 -0500

    add makefile

--- sn25377_hr8742/runkvpaxos.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-javac -cp junit-4.12.jar:hamcrest-core-1.3.jar paxos/*.java kvpaxos/*.java
-java -cp junit-4.12.jar:hamcrest-core-1.3.jar:paxos:. org.junit.runner.JUnitCore kvpaxos.KVPaxosTest
